36606edc4fed20d1c411a3e6f42c0c4cc2cc3faa
JesBanker <107668916+JesBanker@users.noreply.github.com>
2026-04-22 00:42:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-fetch("menui.html")
+fetch("menu.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("afterbegin", html);
-
-    document.querySelector(".menu-btn")
-      .addEventListener("click", toggleMenu);
   });
 
 function toggleMenu() {
-    document.getElementById("menu").classList.toggle("hidden");
+    const menu = document.getElementById("menu");
+
+    if (!menu) {
+        console.log("menu not loaded yet");
+        return;
+    }
+
+    menu.classList.toggle("hidden");
 }
